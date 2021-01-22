8da3d537029124d051a9cb3d4f92f74080182ffa
Veronika Dodda <v.shengeliya@gmail.com>
2021-01-22 13:19:00 -0500

    final commit

--- frontend/src/components/Modal.js
+++ frontend/src/components/Modal.js
@@ -14,6 +14,9 @@
     } from "reactstrap";
 
     export default class CustomModal extends Component {
+
+
+      
       constructor(props) {
         super(props);
         this.state = {
@@ -21,14 +24,23 @@
         };
       }
       handleChange = e => {
+
+        console.log(this.state.activeItem.due_date)
+        console.log(new Date())
         let { name, value } = e.target;
         if (e.target.type === "checkbox") {
           value = e.target.checked;
+
+          // implelemting "G) update 'completed_time' in the db when marking a todo complete"
+          const dateNow = new Date();
+          const activeItem = { ...this.state.activeItem, [this.state.activeItem.due_date]: dateNow };
+          this.setState({ activeItem });
         }
         const activeItem = { ...this.state.activeItem, [name]: value };
         this.setState({ activeItem });
       };
       render() {
+
         const { toggle, onSave } = this.props;
         return (
           <Modal isOpen={true} toggle={toggle}>
@@ -53,6 +65,26 @@
                     value={this.state.activeItem.description}
                     onChange={this.handleChange}
                     placeholder="Enter Todo description"
+                  />
+                </FormGroup>
+                <FormGroup>
+                  <Label for="due">Due date</Label>
+                  <Input
+                    type="text"
+                    name="due_date"
+                    value={this.state.activeItem.due_date}
+                    onChange={this.handleChange}
+                    placeholder={this.state.activeItem.due_date}
+                  />
+                </FormGroup>
+                <FormGroup>
+                  <Label for="priority">Priority</Label>
+                  <Input
+                    type="text"
+                    name="priority"
+                    value={this.state.activeItem.priority}
+                    onChange={this.handleChange}
+                    placeholder="Change the priority"
                   />
                 </FormGroup>
                 <FormGroup check>
